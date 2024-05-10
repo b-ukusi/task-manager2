8b88adf9c52293415d44adf308e24b1cc8b211a0
nana <112271406+b-ukusi@users.noreply.github.com>
2024-05-10 01:30:02 -0700

    rtansition styling

--- public/javascripts/popup.js
+++ public/javascripts/popup.js
@@ -1,5 +1,7 @@
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const openModalButtons2 = document.querySelectorAll('[data-modal-target2]')
+const closeModalButtons2 = document.querySelectorAll('[data-close-button2]')
 const overlay = document.getElementById('overlay')
 //const createClientButton = document.getElementById('create-client-button')
 
@@ -39,5 +41,48 @@ function openModal(modal) {
 function closeModal(modal) {
   if (modal == null) return
   modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
+
+
+
+//  second  model
+openModalButtons2.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal2 = document.querySelector(button.dataset.modalTarget2)
+    openModal2(modal2)
+  })              
+})
+
+overlay.addEventListener('click', () => {
+  const modals2 = document.querySelectorAll('.modal2.active')
+  modals.forEach(modal2 => {
+    closeModal2(modal2)
+  })
+})
+
+closeModalButtons2.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal2 = button.closest('.modal2')
+    closeModal2(modal2)
+  })
+})
+
+
+//createClientButton.addEventListener('click', () => {
+  //const modal = document.getElementById('modal');
+ // closeModal(modal);
+//});
+
+function openModal2(modal2) {
+  if (modal2 == null) return
+  modal2.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal2(modal2) {
+  if (modal2 == null) return
+  modal2.classList.remove('active')
   overlay.classList.remove('active')
 }
--- public/javascripts/popup2.js
+++ public/javascripts/popup2.js
@@ -1,26 +1,26 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const openModalButtons = document.querySelectorAll('[data-modal-target2]')
+const closeModalButtons = document.querySelectorAll('[data-close-button2]')
 const overlay = document.getElementById('overlay')
 //const createClientButton = document.getElementById('create-client-button')
 
 openModalButtons.forEach(button => {
   button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.modalTarget)
-    openModal(modal)
+    const modal2 = document.querySelector(button.dataset.modal2Target)
+    openModal2(modal2)
   })              
 })
 
 overlay.addEventListener('click', () => {
-  const modals = document.querySelectorAll('.modal.active')
-  modals.forEach(modal => {
-    closeModal(modal)
+  const modals2 = document.querySelectorAll('.modal2.active')
+  modals.forEach(modal2 => {
+    closeModal2(modal2)
   })
 })
 
 closeModalButtons.forEach(button => {
   button.addEventListener('click', () => {
-    const modal = button.closest('.modal')
-    closeModal(modal)
+    const modal2 = button.closest('.modal2')
+    closeModal2(modal2)
   })
 })
 
@@ -30,14 +30,14 @@ closeModalButtons.forEach(button => {
  // closeModal(modal);
 //});
 
-function openModal(modal) {
+function openModal2(modal2) {
   if (modal == null) return
-  modal.classList.add('active')
+  modal2.classList.add('active')
   overlay.classList.add('active')
 }
 
-function closeModal(modal) {
+function closeModa2(modal2) {
   if (modal == null) return
-  modal.classList.remove('active')
+  modal2.classList.remove('active')
   overlay.classList.remove('active')
 }
